e52c67f1af20a7baf87421669e573016a788774e
andrewapp <andrew.appuhamy@gmail.com>
2021-02-10 11:54:58 +1100

    section 4.8 done

--- bwgimg.py
+++ bwgimg.py
@@ -2,18 +2,20 @@ import array
 import sys
 from PIL import Image
 from vector import Vec3
+from ray import Ray
+
+
 
 
 
 def main():
-    aspect_ratio = 16/9
+    aspect_ratio = 16 / 9
     img_width = 400
     img_height = int(img_width / aspect_ratio)
     max_val = 255
     ppm_h = f'P6 {img_width} {img_height} {max_val}\n'
     image = array.array('B', [255, 255, 255] * img_width * img_height)
-    col1 = Vec3([1.0, 1.0, 1.0])
-    col2 = Vec3([0.5, 0.7, 1.0])
+
     vp_height = 2.0
     vp_width = aspect_ratio * vp_height
     focal = 1.0
@@ -21,11 +23,11 @@ def main():
     origin = Vec3([0, 0, 0])
     horizontal = Vec3([vp_width, 0, 0])
     vertical = Vec3([0, vp_height, 0])
-    llc = origin.sub(horizontal.divide(2)).sub(vertical.divide(2)).sub(Vec3([0, 0, focal]))
+    llc = Vec3([-2.0, -1.0, -1.0]) #origin - horizontal/2 - vertical/2 - vec3(0, 0, focal_length);
 
     print(f'P6 {img_width} {img_height} {max_val}\n')
     k = 0
-    for j in range(img_height-1, 0, -1):
+    for j in range(img_height - 1, 0, -1):
 
         sys.stderr.write(f'\rScanlines remaining: {j}\n')
         sys.stderr.flush()
@@ -34,23 +36,39 @@ def main():
 
         for i in range(0, img_width, 1):
             index = 3 * (k * img_width + i)
-
+            col1 = [1.0, 1.0, 1.0]
+            col2 = [0.5, 0.7, 1.0]
             u = i / (img_width - 1)
             v = j / (img_height - 1)
+
             r = llc.add(horizontal.multiply_s(u)).add(vertical.multiply_s(v)).sub(origin)
+
             unit_direction = r.unitvec()
             udir = Vec3(unit_direction)
-            a = udir.y()
+            a = unit_direction[1]
             b = a + 1.0
-            t = 0.5*b
+            t = 0.5 * b
             c = 1.0 - t
+            l = 0
 
-            ray_color = col1.multiply_s(c).add(col2.multiply_s(t))
-            image[index] = 255 * int(ray_color.x())
-            image[index + 1] = 255 * int(ray_color.y())
-            image[index + 2] = 255 * int(ray_color.z())
-            print(ray_color)
+            for x in col1:
+                col1[l] = c * x
+                l = l+1
 
+            m = 0
+            for y in col2:
+                col2[m] = t*y
+                m = m+1
+
+            ray_color = [x + y for x, y in zip(col1, col2)]
+            ra = Vec3(ray_color)
+            rr = round(255 * ra.x())
+            rg = round(255 * ra.y())
+            rb = round(255 * ra.z())
+            image[index] = rr
+            image[index + 1] = rg
+            image[index + 2] = rb
+            print(t)
 
     sys.stderr.write(f'\nDone.\n')
     with open("rayimg.ppm", 'wb') as f:
@@ -58,9 +76,7 @@ def main():
         image.tofile(f)
 
 
-
 main()
-
 
 original_image = Image.open("rayimg.ppm")
 original_image.show()
--- ray.py
+++ ray.py
@@ -3,12 +3,12 @@ import math as m
 import array
 class Ray:
     def __init__(self, orig, direct):
-        self.orig = Vec3(orig)
-        self.direct = Vec3(direct)
+        self.orig = orig
+        self.direct = direct
 
 
     def pos(self, t):
-        return Vec3([self.orig.x() + t*self.direct.x(), self.orig.y() + t*self.direct.y(), self.orig.z()+t*self.direct.z()])
+        return self.origin.add(self.direction.multiply_s(t))
 
 
 
