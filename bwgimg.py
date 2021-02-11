abe5952d2776488a38f990d8f98ffd5b4d8e68d0
andrewapp <andrew.appuhamy@gmail.com>
2021-02-11 13:38:58 +1100

    sphere added

--- bwgimg.py
+++ bwgimg.py
@@ -14,17 +14,23 @@ def main():
     img_height = int(img_width / aspect_ratio)
     max_val = 255
     ppm_h = f'P6 {img_width} {img_height} {max_val}\n'
-    image = array.array('B', [255, 255, 255] * img_width * img_height)
+    image = array.array('B', [217, 232, 255] * img_width * img_height)
 
     vp_height = 2.0
-    vp_width = aspect_ratio * vp_height
+    vp_width = round(aspect_ratio * vp_height)
     focal = 1.0
 
     origin = Vec3([0, 0, 0])
     horizontal = Vec3([vp_width, 0, 0])
     vertical = Vec3([0, vp_height, 0])
-    llc = Vec3([-2.0, -1.0, -1.0]) #origin - horizontal/2 - vertical/2 - vec3(0, 0, focal_length);
+    hlc = horizontal.multiply_s(0.5)
+    vlc = vertical.multiply_s(0.5)
+    fl = Vec3([0, 0, focal])
 
+    llc = origin.sub(hlc) #origin - horizontal/2 - vertical/2 - vec3(0, 0, focal_length);
+    llc = llc.sub(vlc)
+    llc = llc.sub(fl)
+    sphere = [255,0,0]
     print(f'P6 {img_width} {img_height} {max_val}\n')
     k = 0
     for j in range(img_height - 1, 0, -1):
@@ -44,7 +50,6 @@ def main():
             r = llc.add(horizontal.multiply_s(u)).add(vertical.multiply_s(v)).sub(origin)
 
             unit_direction = r.unitvec()
-            udir = Vec3(unit_direction)
             a = unit_direction[1]
             b = a + 1.0
             t = 0.5 * b
@@ -62,13 +67,32 @@ def main():
 
             ray_color = [x + y for x, y in zip(col1, col2)]
             ra = Vec3(ray_color)
-            rr = round(255 * ra.x())
-            rg = round(255 * ra.y())
-            rb = round(255 * ra.z())
-            image[index] = rr
-            image[index + 1] = rg
-            image[index + 2] = rb
-            print(t)
+            rayr = round(255 * ra.x())
+            rayg = round(255 * ra.y())
+            rayb = round(255 * ra.z())
+            center = Vec3([0, 0, -1])
+            oc = origin.sub(center)
+            da = r.dot_p(r)
+            db = oc.dot_p(r)
+
+
+            db = 2.0*db
+            dc = oc.dot_p(oc)
+            dc = dc-0.5*0.5
+            discrim = (db**2)-4*da*dc
+
+            if discrim > 0:
+                image[index] = 255
+                image[index + 1] = 0
+                image[index + 2] = 0
+                print(sphere)
+            else:
+                image[index] = rayr
+                image[index + 1] = rayg
+                image[index + 2] = rayb
+                print(ray_color)
+
+
 
     sys.stderr.write(f'\nDone.\n')
     with open("rayimg.ppm", 'wb') as f:
